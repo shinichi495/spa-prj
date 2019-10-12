6f3fd8cc02b5e91c81b8d4b0de481bc49a3b7ca7
ThangTD <tducthang7192@gmail.com>
2019-10-12 12:39:36 +0700

    Add Folder  Images and folder views

--- src/views/CoreViews/Login/Login.js
+++ src/views/CoreViews/Login/Login.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
-
+import Login_Image from '../../../Images/Login_Image.jpg'
+// import { url } from 'inspector';
+const styles ={
+  backgroundImage: {
+    backgroundImage:`url(${Login_Image})`,
+    height: "100%",
+  backgroundPosition: "center",
+ 
+ 
+  }}
 class Login extends Component {
-  login(e) {
-    e.preventDefault();
-    console.log(e);
-    this.props.history.push('/');
-  }
   render() {
     return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
+      <div className="app flex-row align-items-center" style={styles.backgroundImage}>
+        <Container >
+          <Row className="justify-content-center" >
             <Col md="8">
               <CardGroup>
                 <Card className="p-4">
                   <CardBody>
                     <Form>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
+                      <h1>Đăng Nhập</h1>
+                      <p className="text-muted">Đăng Nhập Vào Tài Khoản Của Bạn</p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="icon-user"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="text" placeholder="Username" autoComplete="username" />
+                        <Input type="text" placeholder="Tài Khoản" autoComplete="username" />
                       </InputGroup>
                       <InputGroup className="mb-4">
                         <InputGroupAddon addonType="prepend">
@@ -33,20 +38,29 @@ class Login extends Component {
                             <i className="icon-lock"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="password" placeholder="Password" autoComplete="current-password" />
+                        <Input type="password" placeholder="Mật Khẩu" autoComplete="current-password" />
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="primary" className="px-4" onClick={e => this.login(e)}>Login</Button>
+                          <Button color="primary" className="px-4">Đăng Nhập</Button>
                         </Col>
-                        <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
-                        </Col>
+                        {/* <Col xs="6" className="text-right">
+                          <Button color="link" className="px-0">Quên Mật Khẩu?</Button>
+                        </Col> */}
                       </Row>
                     </Form>
                   </CardBody>
                 </Card>
-                
+                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
+                  <CardBody className="text-center">
+                    <div>
+                      <h2>Giới Thiệu Spa</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                      
+                    </div>
+                  </CardBody>
+                </Card>
               </CardGroup>
             </Col>
           </Row>
--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -5,23 +5,23 @@ import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGr
 class Login extends Component {
   render() {
     return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
+      <div className="app flex-row align-items-center" style={{background:"lightyellow"}}>
+        <Container >
+          <Row className="justify-content-center" >
             <Col md="8">
               <CardGroup>
                 <Card className="p-4">
                   <CardBody>
                     <Form>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
+                      <h1>Đăng Nhập</h1>
+                      <p className="text-muted">Đăng Nhập Vào Tài Khoản Của Bạn</p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="icon-user"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="text" placeholder="Username" autoComplete="username" />
+                        <Input type="text" placeholder="Tài Khoản" autoComplete="username" />
                       </InputGroup>
                       <InputGroup className="mb-4">
                         <InputGroupAddon addonType="prepend">
@@ -29,14 +29,14 @@ class Login extends Component {
                             <i className="icon-lock"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="password" placeholder="Password" autoComplete="current-password" />
+                        <Input type="password" placeholder="Mật Khẩu" autoComplete="current-password" />
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
+                          <Button color="primary" className="px-4">Đăng Nhập</Button>
                         </Col>
                         <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
+                          <Button color="link" className="px-0">Quên Mật Khẩu?</Button>
                         </Col>
                       </Row>
                     </Form>
@@ -45,12 +45,10 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
                   <CardBody className="text-center">
                     <div>
-                      <h2>Sign up</h2>
+                      <h2>Giới Thiệu Spa</h2>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua.</p>
-                      <Link to="/register">
-                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
-                      </Link>
+                      
                     </div>
                   </CardBody>
                 </Card>
